6d2d32b2f787ef92bead3c3b121b3d9e557ef925
samwindham1 <sam.windham95@gmail.com>
2017-11-02 00:41:26 -0600

    added SASS

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,16 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "scss/App";
 </style>
