d3781ad4942b937c53fd30845ce5408dd94a7ed6
samwindham1 <sam.windham95@gmail.com>
2017-11-04 01:36:09 -0600

    new components (next sections)

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer id="footer">
+    <p>{{ temp }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+  data() {
+    return {
+      temp: 'Footer'
+    };
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import "~styles/components/footer";
+</style>
---
+++ src/components/recommend.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="recommend">
+    <h2>{{ temp }}</h2>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'recommend',
+  data() {
+    return {
+      temp: 'Recommend'
+    };
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import "~styles/components/recommend";
+</style>
