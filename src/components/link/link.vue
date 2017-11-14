e2c3723a9459775cb6459b9212733dee7ab2e632
samwindham1 <sam.windham95@gmail.com>
2017-11-13 20:09:22 -0700

    search functionality finished

--- src/components/link/link.vue
+++ src/components/link/link.vue
@@ -3,11 +3,15 @@
     <div class="container">
       <div id="search">
         <!-- Search Box -->
-        <input type="text" name="search" id="search-box" autocomplete="off" placeholder="Search a movie...">
+        <input type="text" name="search" id="search-box" autocomplete="off" placeholder="Search a movie..." v-model="userInput" debounce="3000">
         <!-- Results -->
-        <ul id="results">
-          <li v-for="(movie, index) in results" :key="index">
-            {{ movie.title }} - {{ movie.release_date }}
+        <ul id="results-dropdown" v-if="searching">
+          <li v-for="(movie, index) in results" :key="index" class="dropdown-item">
+            <img :src="getPoster(movie.poster_path)" class="search-image">
+            <div class="movie-info">
+              {{ movie.title }}
+              <span class="date">({{ parseDate(movie.release_date) }})</span>
+            </div>
           </li>
         </ul>
       </div>
@@ -17,26 +21,82 @@
 
 <script>
 import { TMDB } from '@/router/http';
+import path from 'path';
+import _ from 'lodash';
 
 export default {
   name: 'link',
   data() {
     return {
+      userInput: '',
+
+      // constants
+      searching: false,
+      search_limit: 4,
+      max_title_length: 50,
+
+      // api results
       results: [],
+      config: [],
       errors: []
     };
   },
 
   created() {
-    TMDB.search('Mad Max')
+    TMDB.config()
       .then(res => {
-        this.results = res.data.results;
-        console.log(res.data.results);
+        this.config = res.data;
       })
       .catch(e => {
         this.errors.push(e);
         console.log(e);
-      })
+      });
+  },
+
+  methods: {
+    parseDate: function (dateString) {
+      return new Date(Date.parse(dateString)).getFullYear();
+    },
+    search: function (query) {
+      this.searching = true;
+      TMDB.search(query)
+        .then(res => {
+          this.results = res.data.results.map((movie) => {
+            if (movie.title.length > this.max_title_length) {
+              movie.title = movie.title.slice(0, this.max_title_length).trim() + '...';
+            }
+            return movie;
+          });
+          // limit results to 4
+          this.results = this.results.slice(0, this.search_limit);
+        })
+        .catch(e => {
+          this.errors.push(e);
+          console.log(e);
+        });
+    },
+    getPoster: function (imgPath) {
+      if (imgPath) {
+        return path.join(this.config.images.secure_base_url, this.config.images.poster_sizes[0], imgPath);
+      }
+      return '';
+    }
+  },
+
+  watch: {
+    userInput: _.debounce(function (val) {
+      if (val) {
+        this.search(val);
+      }
+      else {
+        this.searching = false;
+      }
+    }, 300),
+    searching: function () {
+      if (!this.searching) {
+        this.results = [];
+      }
+    }
   }
 
 };
--- src/router/http.js
+++ src/router/http.js
@@ -1,20 +1,21 @@
 import axios from 'axios';
 
-var APIkey = '51ce10e2d314a7fe78bf390de03163a2';
+const APIkey = '51ce10e2d314a7fe78bf390de03163a2';
 
 const HTTP = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
     'api_key': APIkey,
-  }
+  },
 })
 
 export const TMDB = {
   get: (movie) => HTTP.get('/movie/' + movie),
-  search: (query) => HTTP.get('/search/movie/', {
+  search: (query) => HTTP('/search/movie', {
     params: {
       query: query,
       page: 1,
     },
-  })
+  }),
+  config: () => HTTP.get('/configuration'),
 }
