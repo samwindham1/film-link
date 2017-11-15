fb21984f6cefe22a4eeddecb38dbcd5825ba78db
samwindham1 <sam.windham95@gmail.com>
2017-11-15 00:45:44 -0700

    Switched dropdown search to vue-multiselect

--- src/components/link/link.vue
+++ src/components/link/link.vue
@@ -3,17 +3,11 @@
     <div class="container">
       <div id="search">
         <!-- Search Box -->
-        <input type="text" name="search" id="search-box" autocomplete="off" placeholder="Search a movie..." v-model="userInput" debounce="3000">
-        <!-- Results -->
-        <ul id="results-dropdown" v-if="searching">
-          <li v-for="(movie, index) in results" :key="index" class="dropdown-item">
-            <img :src="getPoster(movie.poster_path)" class="search-image">
-            <div class="movie-info">
-              {{ movie.title }}
-              <span class="date">({{ parseDate(movie.release_date) }})</span>
-            </div>
-          </li>
-        </ul>
+        <multiselect v-model="selected_movie" id="search-box" placeholder="Search a movie" open-direction="down" label="title" :options="results" :multiple="false" :loading="isLoading" :internal-search="false" :options-limit="50" :max-height="300" @search-change="search" @close="clearResults"></multiselect>
+        <!-- Selected -->
+        <div class="selected" v-if="selected_movie">
+          {{selected_movie.title}}
+        </div>
       </div>
     </div>
   </main>
@@ -22,18 +16,19 @@
 <script>
 import { TMDB } from '@/router/http';
 import path from 'path';
-import _ from 'lodash';
+import _, { debounce } from 'lodash';
+import multiselect from 'vue-multiselect';
 
 export default {
   name: 'link',
+  components: {
+    multiselect,
+  },
   data() {
     return {
-      userInput: '',
-
-      // constants
-      searching: false,
-      search_limit: 4,
-      max_title_length: 50,
+      // multi-select
+      selected_movie: null,
+      isLoading: false,
 
       // api results
       results: [],
@@ -57,53 +52,38 @@ export default {
     parseDate: function (dateString) {
       return new Date(Date.parse(dateString)).getFullYear();
     },
-    search: function (query) {
-      this.searching = true;
+    search: debounce(function (query) {
+      if (!query) return;
+      console.log('debounce search');
+      this.isLoading = true;
       TMDB.search(query)
         .then(res => {
-          this.results = res.data.results.map((movie) => {
-            if (movie.title.length > this.max_title_length) {
-              movie.title = movie.title.slice(0, this.max_title_length).trim() + '...';
-            }
-            return movie;
-          });
-          // limit results to 4
-          this.results = this.results.slice(0, this.search_limit);
+          this.results = res.data.results;
+          this.isLoading = false;
         })
         .catch(e => {
           this.errors.push(e);
           console.log(e);
+          this.isLoading = false;
         });
-    },
+    }, 300),
     getPoster: function (imgPath) {
       if (imgPath) {
         return path.join(this.config.images.secure_base_url, this.config.images.poster_sizes[0], imgPath);
       }
       return '';
+    },
+    clearResults: function () {
+      this.results = [];
     }
   },
-
-  watch: {
-    userInput: _.debounce(function (val) {
-      if (val) {
-        this.search(val);
-      }
-      else {
-        this.searching = false;
-      }
-    }, 300),
-    searching: function () {
-      if (!this.searching) {
-        this.results = [];
-      }
-    }
-  }
-
 };
 
 </script>
 
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "~styles/components/link";
 </style>
+
