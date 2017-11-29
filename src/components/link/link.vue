c43dcd9d39e79d43c888df5ed692e19673fa3faa
samwindham1 <sam.windham95@gmail.com>
2017-11-29 16:30:07 -0700

    seperated search into new component

--- src/components/link/link.vue
+++ src/components/link/link.vue
@@ -1,105 +1,27 @@
 <template>
   <main id="link">
-
-    <div id="search">
-      <div class="container">
-        <!-- Search Box -->
-        <multiselect v-model="selected" id="search-box" placeholder="Search a movie" open-direction="down" label="title" :options="results" :multiple="true" :loading="isLoading" :internal-search="false" :options-limit="50" :max-height="150" @search-change="search" @close="clearResults"></multiselect>
-      </div>
-    </div>
-
-    <!-- Selected -->
-    <div class="selected" v-if="selected && selected.length > 0">
-      <div class="container">
-        <ul>
-          <li class="selected-movie" v-for="movie in selected" :key="movie.id" :title="movie.title">
-            <img class="poster" :src="getPoster(movie.poster_path)">
-            <div class="dim-overlay"></div>
-            <i class="fa fa-times-thin close-icon"></i>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <!-- Search Component -->
+    <search></search>
 
     <div class="divider"></div>
 
+    <!-- Home Page Animation -->
     <div id="neural-animation"></div>
+
   </main>
 </template>
 
 <script>
-import { TMDB } from '@/router/http';
-import path from 'path';
-import _, { debounce } from 'lodash';
-import multiselect from 'vue-multiselect';
+import search from './search';
 
 export default {
   name: 'link',
   components: {
-    multiselect,
+    search,
   },
-  data() {
-    return {
-      // multi-select
-      selected: null,
-      isLoading: false,
-
-      // api results
-      results: [],
-      config: [],
-      errors: []
-    };
-  },
-
-  created() {
-    TMDB.config()
-      .then(res => {
-        this.config = res.data;
-      })
-      .catch(e => {
-        this.errors.push(e);
-        console.log(e);
-      });
-  },
-
-  methods: {
-    parseDate: function (dateString) {
-      return new Date(Date.parse(dateString)).getFullYear();
-    },
-
-    search: debounce(function (query) {
-      if (!query) return;
-
-      this.isLoading = true;
-      TMDB.search(query)
-        .then(res => {
-          this.results = res.data.results;
-          this.isLoading = false;
-        })
-        .catch(e => {
-          this.errors.push(e);
-          console.log(e);
-          this.isLoading = false;
-        });
-    }, 300),
-
-    getPoster: function (imgPath) {
-      if (imgPath) {
-        return path.join(this.config.images.secure_base_url, this.config.images.poster_sizes[2], imgPath);
-      }
-      return '';
-    },
-
-    clearResults: function () {
-      this.results = [];
-    }
-  },
-
 };
-
 </script>
 
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style lang="scss">
   @import "~styles/components/link";
---
+++ src/components/link/search.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="">
+    <div id="search">
+      <div class="container">
+        <!-- Search Box -->
+        <multiselect v-model="selected" id="search-box" placeholder="Search a movie" open-direction="down" label="title" :options="results" :multiple="true" :loading="isLoading" :internal-search="false" :options-limit="50" :max-height="150" @search-change="search" @close="clearResults"></multiselect>
+      </div>
+    </div>
+
+    <!-- Selected -->
+    <div class="selected" v-if="selected && selected.length > 0">
+      <div class="container">
+        <ul>
+          <li class="selected-movie" v-for="movie in selected" :key="movie.id" :title="movie.title">
+            <img class="poster" :src="getPoster(movie.poster_path)">
+            <div class="dim-overlay"></div>
+            <i class="fa fa-times-thin close-icon" aria-hidden="true"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { TMDB } from '@/router/http';
+import path from 'path';
+import _, { debounce } from 'lodash';
+import multiselect from 'vue-multiselect';
+
+export default {
+  name: 'search',
+  components: {
+    multiselect,
+  },
+  data() {
+    return {
+      // multi-select
+      selected: null,
+      isLoading: false,
+
+      // api results
+      results: [],
+      config: [],
+      errors: []
+    };
+  },
+  created() {
+    TMDB.config()
+      .then(res => {
+        this.config = res.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+        console.log(e);
+      });
+  },
+
+  methods: {
+    parseDate: function (dateString) {
+      return new Date(Date.parse(dateString)).getFullYear();
+    },
+
+    search: debounce(function (query) {
+      if (!query) return;
+
+      this.isLoading = true;
+      TMDB.search(query)
+        .then(res => {
+          this.results = res.data.results;
+          this.isLoading = false;
+        })
+        .catch(e => {
+          this.errors.push(e);
+          console.log(e);
+          this.isLoading = false;
+        });
+    }, 300),
+
+    getPoster: function (imgPath) {
+      if (imgPath) {
+        console.log(this.config.images.poster_sizes[3]);
+        return path.join(this.config.images.secure_base_url, this.config.images.poster_sizes[3], imgPath);
+      }
+      return '';
+    },
+
+    clearResults: function () {
+      this.results = [];
+    }
+  },
+}
+</script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style lang="scss">
+  @import "~styles/components/search";
+</style>
+
